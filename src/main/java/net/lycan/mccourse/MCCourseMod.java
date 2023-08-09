2adc052ce49eed798331e616d237586785fe98f8
Lycan <aidenmorrow27@gmail.com>
2023-08-09 20:52:22 +1000

    added comment

--- src/main/java/net/lycan/mccourse/MCCourseMod.java
+++ src/main/java/net/lycan/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.lycan.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lycan.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ public class MCCourseMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-	//hi
+		ModItems.registerModItems();
 
 	}
 }
