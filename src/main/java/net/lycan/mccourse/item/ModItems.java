3e64c8cc8ff059b0f97a7ca825755e54c6fcc9e8
Lycan <aidenmorrow27@gmail.com>
2023-08-10 09:10:48 +1000

    added comment

--- src/main/java/net/lycan/mccourse/item/ModItems.java
+++ src/main/java/net/lycan/mccourse/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.lycan.mccourse.MCCourseMod;
+import net.lycan.mccourse.block.ModBlocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -17,12 +18,17 @@ public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet",
             new Item(new FabricItemSettings()));
 
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet",
+            new Item(new FabricItemSettings()));
+
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, new Identifier(MCCourseMod.MOD_ID, name), item);
     }
 
     private static void itemGroupIngredients(FabricItemGroupEntries entries) {
     entries.add(PINK_GARNET);
+    entries.add(RAW_PINK_GARNET);
+    entries.add(ModBlocks.PINK_GARNET_BLOCK);
     }
     public static void registerModItems(){
         MCCourseMod.LOGGER.info("Registering Mod Items For" + MCCourseMod.MOD_ID);
