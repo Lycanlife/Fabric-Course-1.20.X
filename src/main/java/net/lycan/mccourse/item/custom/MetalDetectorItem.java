c834326949996857ac3683e9d763cab63db2e1f7
Lycan <aidenmorrow27@gmail.com>
2023-08-10 11:47:23 +1000

    added comment

--- src/main/java/net/lycan/mccourse/item/custom/MetalDetectorItem.java
+++ src/main/java/net/lycan/mccourse/item/custom/MetalDetectorItem.java
@@ -3,13 +3,19 @@ package net.lycan.mccourse.item.custom;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
@@ -52,5 +58,14 @@ public class MetalDetectorItem extends Item {
     private boolean isValuableBlock(BlockState blockState) {
         return blockState.getBlock() == Blocks.IRON_ORE||blockState.getBlock() == Blocks.DEEPSLATE_DIAMOND_ORE
                 ||blockState.getBlock() == Blocks.GOLD_ORE||blockState.getBlock() == Blocks.DIAMOND_ORE;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()){
+            tooltip.add(Text.translatable("tooltip.mccourse.metaldetector.shift"));
+        }else{
+            tooltip.add(Text.translatable("tooltip.mccourse.metaldetector"));
+        }
     }
 }
